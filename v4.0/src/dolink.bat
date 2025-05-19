bab8580ab2b9591af8218a63b36d3e453cb09559
Ray Chason <Ray Chason>
2025-05-18 22:06:02 -0400

    Some cleanup

--- v4.0/src/dolink.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd %1
-set LIB=\src\tools\bld\lib
-\src\tools\link %2 %3 %4 %5 %6 %7 %8 %9
--- v4.0/src/domsc2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd %1
-set INCLUDE=\src\tools\bld\inc
-\src\tools\cl -c -AS -Od -Zp -I. -I\src\h -D__cdecl= -Fo%3 %2
